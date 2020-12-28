5522a3506840643995926cc7805f0f722c2dbb8a
Daniel Winterstein <daniel@good-loop.com>
2020-12-28 14:40:07 +0000

    build-deps

--- builder/com/winterwell/calstat/BuildMoneyscript.java
+++ builder/com/winterwell/calstat/BuildMoneyscript.java
@@ -1,5 +1,9 @@
 package com.winterwell.calstat;
 
+import java.util.List;
+
+import com.winterwell.bob.BuildTask;
+import com.winterwell.bob.tasks.MavenDependencyTask;
 import com.winterwell.bob.wwjobs.BuildWinterwellProject;
 
 public class BuildMoneyscript extends BuildWinterwellProject {
@@ -10,4 +14,18 @@ public class BuildMoneyscript extends BuildWinterwellProject {
 //		setMakeFatJar(true); // odd classpath issues -- this is one (not ideal but simple) way to fix them
 	}
 
+	@Override
+	public List<BuildTask> getDependencies() {
+		List<BuildTask> bts = super.getDependencies();
+		
+		MavenDependencyTask mdt = new MavenDependencyTask();
+		mdt.addDependency("com.google.api-client:google-api-client:1.30.4");
+		mdt.addDependency("com.google.oauth-client:google-oauth-client-jetty:1.30.6");
+		mdt.addDependency("com.google.apis:google-api-services-sheets:v4-rev581-1.25.0");
+		mdt.setIncSrc(true);
+		bts.add(mdt);
+		
+		return bts;
+	}
+	
 }
